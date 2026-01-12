ac00e658b2d02c3e825ccb7dff59e5eba7cb5ee2
Gabriel <gabrielfds@gmail.com>
2026-01-12 12:27:22 -0300

    Primeiro Commit

---
+++ frontend/portal/dados.js
@@ -0,0 +1,14 @@
+const tags = [
+  {
+    nome: "João Silva",
+    condicao: "Autismo",
+    telefone: "(11) 99999-9999",
+    mensagem: "Meu filho precisa de ajuda para voltar para casa."
+  },
+  {
+    nome: "Ana Silva",
+    condicao: "Alergia",
+    telefone: "(11) 98888-8888",
+    mensagem: "Minha filha tem alergia a amendoim."
+  }
+];
---
+++ public-tag/dados-publicos.js
@@ -0,0 +1,6 @@
+const dados = {
+  nome: "João Silva",
+  condicao: "Autismo",
+  telefone: "(11) 99999-9999",
+  mensagem: "Meu filho precisa de ajuda para voltar para casa."
+};
